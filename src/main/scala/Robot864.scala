8bfb7528234f83a44128764421982adb9a517980
JoshZF <joshz.fong@gmail.com>
2018-01-27 18:54:26 -0800

    forward and turn control updated

--- src/main/scala/Robot864.scala
+++ src/main/scala/Robot864.scala
@@ -18,35 +18,34 @@ class Robot864 extends IterativeRobot {
   implicit val props = Signal.constant(new DrivetrainProperties)
   val drivetrainComponent = new Drivetrain.DrivetrainComponent
 
-  val absoluteTargetDistance = drivetrainHardware.forwardPosition.zipAsync(targeting.distanceToTarget.map(p => {
-    if (math.random > 0.99) println(p)
-    p.map(x => Feet(2) - x).getOrElse(Feet(0))
-  })).map(t => t._1 + t._2)
-
-  val forwardPositionControlling = Drivetrain.UnicycleControllers.forwardPositionControl(absoluteTargetDistance)._1
-
-  val xOffsets = targeting.angleToTarget.map(p => -p)
-
-  val turnPositionControlling = drivetrainHardware.turnPosition.zipAsync(xOffsets).map(t => t._1 + t._2)
-  val turnController = Drivetrain.UnicycleControllers.turnPositionControl(turnPositionControlling)._1
-
-  val forwardController = Drivetrain.UnicycleControllers.lowerLevelVelocityControl(forwardPositionControlling)
-
-  val out = Drivetrain.UnicycleControllers.lowerLevelOpenLoop(
-    forwardPositionControlling.map(p =>
-      p.forward
-    ).zip(turnController.map(p =>
-      p.turn
-    )).map(p =>
-      UnicycleSignal(p._1 max Percent(-10) min Percent(10), p._2 max Percent(-10) min Percent(10))
-    )
-  )
-
 //  val printer = targeting.distanceToTarget.foreach(p => println("distance:", p))
 //  val secondPrinter = drivetrainHardware.forwardVelocity.foreach(p => println("vel:", p))
 
   override def teleopInit(): Unit = {
     drivetrainComponent.resetToDefault()
+    val absoluteTargetDistance = drivetrainHardware.forwardPosition.zipAsync(targeting.distanceToTarget.map(p => {
+      if (math.random > 0.99) println(p)
+      p.map(x => Feet(2) - x).getOrElse(Feet(0))
+    })).map(t => t._1 + t._2)
+
+    val forwardPositionControlling = Drivetrain.UnicycleControllers.forwardPositionControl(absoluteTargetDistance)._1
+
+    val xOffsets = targeting.angleToTarget.map(p => -p)
+
+    val turnPositionControlling = drivetrainHardware.turnPosition.zipAsync(xOffsets).map(t => t._1 + t._2)
+    val turnController = Drivetrain.UnicycleControllers.turnPositionControl(turnPositionControlling)._1
+
+    val forwardController = Drivetrain.UnicycleControllers.lowerLevelVelocityControl(forwardPositionControlling)
+
+    val out = Drivetrain.UnicycleControllers.lowerLevelOpenLoop(
+      forwardPositionControlling.map(p =>
+        p.forward
+      ).zip(turnController.map(p =>
+        p.turn
+      )).map(p =>
+        UnicycleSignal(p._1 max Percent(-20) min Percent(20), p._2 max Percent(-20) min Percent(20))
+      )
+    )
 
     drivetrainComponent.setController(out.withCheck(out =>
       if (math.random > 0.99) println(out)
